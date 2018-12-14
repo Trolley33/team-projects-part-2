d2fbc0afbc0801cd4100d74cad80133aedb99b86
Reece <reecetrolley@gmail.com>
2018-12-14 18:41:33 +0000

    Specialist Page Started
    Began a bit of work on specialist page, just getting the assigned problems for that user to show up; links are currently broken for homepage. Will probably stop working fairly soon to leave some things to do after the holidays.

--- resources/views/pages/operator/homepage.blade.php
+++ resources/views/pages/operator/homepage.blade.php
@@ -13,37 +13,42 @@
 
         <div class="w3-container w3-white login w3-mobile" style='text-align:center'>
             <h3>Database Management Options</h3><hr />
-            <a class="blank" href="problems/">
+            <a class="blank" href="/problems/">
                 <div class="menu-item w3-card w3-button w3-row">
                     Problem Manager
                 </div>
             </a><br />
-            <a class="blank" href="users/">
+            <a class="blank" href="/users/">
                 <div class="menu-item w3-card w3-button w3-row">
                     User Manager
                 </div>
             </a><br />
-            <a class="blank" href="departments/">
+            <a class="blank" href="/departments/">
                 <div class="menu-item w3-card w3-button w3-row">
                     Department Manager
                 </div>
             </a><br />
-            <a class="blank" href="jobs/">
+            <a class="blank" href="/jobs/">
                 <div class="menu-item w3-card w3-button w3-row">
                     Job Manager
                 </div>
             </a><br />
-            <a class="blank" href="equipment/">
+            <a class="blank" href="/equipment/">
                 <div class="menu-item w3-card w3-button w3-row">
                     Equipment Manager
                 </div>
             </a><br />
-            <a class="blank" href="software/">
+            <a class="blank" href="/software/">
                 <div class="menu-item w3-card w3-button w3-row">
                     Software Manager
                 </div>
             </a><br />
           </div>
+          <a class="blank" href="/problem_types/">
+                <div class="menu-item w3-card w3-button w3-row">
+                    Problem Type Manager
+                </div>
+            </a><br />
         <br />
     </div>
 
--- resources/views/pages/specialist/homepage.blade.php
+++ resources/views/pages/specialist/homepage.blade.php
@@ -1,51 +1,38 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="w3-container w3-white login w3-mobile">
-
-    <div class="w3-padding w3-white" style='text-align:center'>
-        <h3>Problem Related Tasks</h3><hr />
-        <a class="blank" href="problems/">
-            <div class="menu-item w3-card w3-button w3-row">
-                View All Problems
-            </div>
-        </a><br />
-    </div>
-    <br />
-    <div class="w3-padding w3-white" style='text-align:center'>
-        <h3>Assigned Problems</h3><hr />
-        <table>
+<div class="w3-white w3-mobile" style="max-width: 1000px;padding: 20px 20px; margin: 50px auto;">
+    <h2>Assigned Problems for {{$user->forename}} {{$user->surname}}</h2>
+    <table id='problems-table' class="display cell-border stripe hover" style="width:100%;">
+        <thead>
             <tr>
-                <th> Timestamp </th>
-                <th> Caller ID </th>
-                <th> Problem Type </th>
-                <th> Notes </th>
+                <th>Problem Logged At</th><th>Problem Type</th><th>Problem Description</th><th>Initial Caller</th><th>---</th>
             </tr>
-            <tr id="1" class="w3-hover-light-grey solve">
-                <td> 2018-10-24 17:55 </td>
-                <td> 0001 </td>
-                <td> Unresponsive Keyboard </td>
-                <td> Not able to type on keyboard properly. </td>
+        </thead>
+        <tbody>
+            @foreach ($problems as $problem)
+            <tr>
+                <td>{{$problem->created_at}}</td><td>@if ($problem->pDesc != '0') ({{$problem->pDesc}}) @endif {{$problem->ptDesc}}</td><td>{{$problem->description}}</td><td>{{$problem->forename}} {{$problem->surname}}</td>
+                <td class="editbutton" onclick="window.location.href = '/problems/{{$problem->id}}';" style="text-align: center;">
+                    View/Edit
+                </td>
             </tr>
+            @endforeach
 
-        </table>
-    </div>
+        </tbody>
+    </table>
+
 </div>
 
 <script>
-    $(document).ready(function() 
-    {
-        $('#back-btn').html("Logout");
-        $('#back-btn').click(function()
-        {
-            window.location.replace('/logout');
-        })
-    });
-    
-    $('tr.solve').click(function() {
-        location.href = "";
-    })
+$(document).ready( function () 
+{
+    var table = $('#problems-table').DataTable();
 
+    // If we provide some sort of search term through the redirect, search it here.
+    var search = "<?php if (session('search')) echo session('search'); ?>";
+    table.search(search).draw();
+});
 </script>
 
 @endsection
