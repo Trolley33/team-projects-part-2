cb9a706379e8a8ccdcb2bf9400d86209da7cbdbd
Reece <reecetrolley@gmail.com>
2019-02-08 13:17:06 +0000

    Comment ProblemController: index->update
    Comment ProblemTypeController@index
    
    Comment ProblemTypeController@create
    
    Comment ProblemTypeController@store
    
    Comment ProblemTypeController@show
    
    Comment ProblemTypeController@show_compact
    
    Comment ProblemController@edit
    
    Comment ProblemController@update

--- app/Http/Controllers/ProblemTypeController.php
+++ app/Http/Controllers/ProblemTypeController.php
@@ -19,11 +19,13 @@ class ProblemTypeController extends Controller
      */
     public function index()
     {
-
+        // If user has operator access.
         if (PagesController::hasAccess(1))
         {
+            // Parent problem types displayed here, and their sub problem types are displayed when a parent problem is viewed.
             $parents = ProblemType::leftJoin('speciality', 'speciality.problem_type_id', '=', 'problem_types.id')->leftJoin('problems', 'problems.problem_type', '=', 'problem_types.id')->selectRaw('problem_types.*, IFNULL(COUNT(speciality.specialist_id),0) as specialists, IFNULL(COUNT(problems.id),0) as problems')->where('problem_types.parent', '=', '-1')->groupBy('problem_types.id')->get();
 
+            // Supply data to view.
             $data = array(
                 'title' => "Speciality Viewer",
                 'desc' => "View information on specialist's problem types.",
@@ -45,9 +47,12 @@ class ProblemTypeController extends Controller
      */
     public function create()
     {
+        // If user has operator access.
         if (PagesController::hasAccess(1))
         {
+            // Get all parent problem types.
             $types = ProblemType::where('parent', '=', '-1')->get();
+            // By default don't select anything in particular.
             $selected = null;
 
             // If specified, grab the specified parent problem type,
@@ -58,6 +63,7 @@ class ProblemTypeController extends Controller
 
             }
 
+            // Supply data to view.
             $data = array(
                 'title' => "Create New Problem Type",
                 'desc' => "For making a new problem type.",
@@ -80,22 +86,25 @@ class ProblemTypeController extends Controller
      */
     public function store(Request $request)
     {
+        // If user has operator access.
         if (PagesController::hasAccess(1))
         {
+            // Make sure required data is supplied.
             $this->validate($request, [
                 'parent-select' => 'required',
                 'desc' => 'required'
             ]);
-
+            // Check no problem type with this description already exists.
             $result = ProblemType::where('description', $request->input('desc'))->get();
-
             if (count($result) == 0)
             {
+                // Create new problem type object, with given information.
                 $problem_type = new ProblemType();
                 $problem_type->parent = $request->input('parent-select');
                 $problem_type->description = $request->input('desc');
                 $problem_type->save();
 
+                // Redirect based on if parent type made or sub-type.
                 if ($request->input('parent-select') == '-1')
                 {
                     return redirect('/problem_types')->with('success', 'Problem Type Created');
@@ -120,13 +129,17 @@ class ProblemTypeController extends Controller
      */
     public function show($id)
     {
+        // If user has operator access.
         if (PagesController::hasAccess(1))
         {
+            // Get problem object from ID, check it exists.
             $problem_type = ProblemType::find($id);
             if (!is_null($problem_type))
             {
+                // Get all specialists with this problem type.
                 $specialists = Speciality::join('users', 'users.id', '=', 'speciality.specialist_id')->join('problem_types', 'problem_types.id', '=', 'speciality.problem_type_id')->where('problem_types.id', '=', $id)->orWhere('problem_types.parent', '=', $id)->get();
 
+                // If type is parent, get sub types and show relevant page.
                 if ($problem_type->parent == '-1')
                 {
                     $types = ProblemType::leftJoin('speciality', 'speciality.problem_type_id', '=', 'problem_types.id')->leftJoin('problems', 'problems.problem_type', '=', 'problem_types.id')->selectRaw('problem_types.*, IFNULL(COUNT(speciality.specialist_id),0) as specialists, IFNULL(COUNT(problems.id),0) as problems')->where('problem_types.parent', '=', $id)->groupBy('problem_types.id')->get();
@@ -143,6 +156,7 @@ class ProblemTypeController extends Controller
 
                     return view('problem_types.show_parent')->with($data);
                 }
+                // If type is subtype, get parent and show relevant page.
                 else
                 {
                     $parent = ProblemType::find($problem_type->parent);
@@ -170,15 +184,20 @@ class ProblemTypeController extends Controller
      */
     public function show_compact($id)
     {
+        // If operator or specialist are viewing.
         if (PagesController::hasAccess(1) || PagesController::hasAccess(2))
         {
+            // Get problem type from ID, check if it exists.
             $type = ProblemType::find($id);
             if (!is_null($type))
             {
+                // Get all specialists with this problem type.
                 $specialists = Speciality::join('users', 'users.id', '=', 'speciality.specialist_id')->join('problem_types', 'problem_types.id', '=', 'speciality.problem_type_id')->where('problem_types.id', '=', $id)->orWhere('problem_types.parent', '=', $id)->get();
 
+                // Find parent (or null if none).
                 $parent = ProblemType::find($type->parent);
 
+                // Get who is viewing page, and ultimately their access level. This is done to prevent specialist from having option to click certain links.
                 $viewer = PagesController::getCurrentUser();
                 $job = Job::find($viewer->job_id);
                 $level = $job->access_level;
@@ -208,11 +227,14 @@ class ProblemTypeController extends Controller
      */
     public function edit($id)
     {
+        // If user has operator access.
         if (PagesController::hasAccess(1))
         {
+            // Get problem type object, check it exists.
             $problem_type = ProblemType::find($id);
             if (!is_null($problem_type))
             {
+                // Get sub problem types if applicable.
                 $types = ProblemType::where('parent', '=', '-1')->get();
                 $data = array(
                     'title' => "Edit Existing Problem Type",
@@ -223,6 +245,7 @@ class ProblemTypeController extends Controller
                     'active' => 'Problem Types'
                 );
 
+                // Redirect based on parent/child.
                 if ($problem_type->parent == -1)
                 {
                     return view('problem_types.edit_parent')->with($data);
@@ -247,15 +270,22 @@ class ProblemTypeController extends Controller
      */
     public function update(Request $request, $id)
     {
+        // Get type object, check it exists.
         $problem_type = ProblemType::find($id);
         if (!is_null($problem_type))
         {
+            // If user has operator access.
             if (PagesController::hasAccess(1))
             {
-                $result = ProblemType::where('description', $request->input('desc'))->where('id', '!=', $id)->get();
+                // If description is blank, use previous description.
+                $desc = $request->input('desc') ?? $problem_type->description;
+
+                // Find duplicate types with the provided the description.
+                $result = ProblemType::where('description', $desc)->where('id', '!=', $id)->get();
 
                 if (count($result) == 0)
                 {
+                    // These fields are required, as otherwise the dropdown was modified in some way.
                     $this->validate($request, [
                         'isParent' => 'required',
                         'parent-select' => 'required',
@@ -263,14 +293,14 @@ class ProblemTypeController extends Controller
                     ]);
 
 
-                    // Caller account.
+                    // Parent problem.
                     if ($request->input('isParent') == 'true')
                     {
                         $problem_type->description = $request->input('desc');
                         $problem_type->save();
                     }
 
-                    // System account.
+                    // Child problem.
                     elseif ($request->input('isParent') == 'false')
                     {
                         $problem_type->parent = $request->input('parent-select');
@@ -289,7 +319,6 @@ class ProblemTypeController extends Controller
             }
             return redirect('login')->with('error', 'Please log in first.');
         }
-
         return redirect('/problem_types')->with('error', 'Sorry, something went wrong.');
     }
 
