34dbb849b34808334621a2cbd49bf8c4dd49aacf
Reece <reecetrolley@gmail.com>
2019-02-08 14:31:48 +0000

    Comment ReassignmentController

--- app/Http/Controllers/ReassignmentController.php
+++ app/Http/Controllers/ReassignmentController.php
@@ -20,28 +20,36 @@ use App\Reassignments;
 
 class ReassignmentController extends Controller
 {
+    /**
+     * Modifies database so problem must be reassigned, and specialist can no longer view it.
+     */
     public function request_reassignment(Request $request, $id)
     {
+        // If user has specialist access.
         if (PagesController::hasAccess(2))
         {
+            // Check reason has been supplied.
             $this->validate($request, [
                 'reason' => 'required'
             ]);
 
+            // Get problem object from ID, and check it exists.
             $problem = Problem::find($id);
-            
             if (is_null($problem))
             {
                 return redirect('/specialist')->with('success', 'Sorry, something went wrong.');
             }
 
+            // Create new reassignment entry.
             $assign = new Reassignments();
             $assign->problem_id = $problem->id;
             $assign->specialist_id = $problem->assigned_to;
             $assign->reason = $request->input('reason');
+            // Not reassigned to anyone yet.
             $assign->reassigned_to = 0;
             $assign->save();
 
+            // No longer assigned to anyone.
             $problem->assigned_to = 0;
             $problem->save();
 
